81757f31277151c416dcc70f63703afcefdac2ac
Alexey Tourbin <alexey.tourbin@gmail.com>
2016-11-23 15:06:37 +0300

    sufsort.c: Manber-Meyers algorithm, 16s instead of 37s
    And the first pass, which sort by 16 bytes takes 15 seconds.
    There's no further room for optimization.

--- sufsort.c
+++ sufsort.c
@@ -1,19 +1,42 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <endian.h>
+#include "sufsort.h"
+#include "qsort.h"
 
-static int sufcmp(const void *pstr1, const void *pstr2)
+static inline bool less(int i, int j, uint64_t *pos, int gap, int len)
 {
-    return strcmp(*(const char **) pstr1, *(const char **) pstr2);
+    if (pos[i] != pos[j])
+	return pos[i] < pos[j];
+    i += gap;
+    j += gap;
+    return (i < len && j < len) ? pos[i] < pos[j] : i > j;
 }
 
 void sufsort(const char *str, int len, unsigned *sa)
 {
-    const char *suf[len];
-    for (int i = 0; i < len; i++)
-	suf[i] = str + i;
-    qsort(suf, len, sizeof *suf, sufcmp);
-    for (int i = 0; i < len; i++)
-	sa[i] = suf[i] - str;
+    uint64_t pos[len];
+    for (int i = 0; i < len; i++) {
+	sa[i] = i;
+	uint64_t u = *(uint64_t *) &str[i];
+	pos[i] = htobe64(u);
+    }
+    int gap = 8;
+    while (1) {
+#define LT(i,j) less(*i,*j, pos, gap, len)
+	QSORT(unsigned, sa, len, LT);
+	unsigned tmp[len];
+	tmp[0] = 0;
+	for (int i = 1; i < len; i++)
+	    tmp[i] = tmp[i-1] + less(sa[i-1], sa[i], pos, gap, len);
+	if (tmp[len-1] == len-1)
+	    break;
+	for (int i = 0; i < len; i++)
+	    pos[sa[i]] = tmp[i];
+	gap += gap;
+    }
 }
 
 /* ex: set ts=8 sts=4 sw=4 noet: */
--- sufsort.h
+++ sufsort.h
@@ -1,4 +1,5 @@
 #ifndef SUFSORT_H_
 #define SUFSORT_H_
+/* reads up to 7 bytes past the end of string */
 void sufsort(const char *str, int len, unsigned *sa);
 #endif
