28c51e538833480f3cb3d381afb3be1f0424acfb
Alexey Tourbin <alexey.tourbin@gmail.com>
2016-12-01 15:16:20 +0300

    added --min-count=N option

--- substrings.c
+++ substrings.c
@@ -13,9 +13,12 @@
  * away junk data.  You can refine the model later, provided that
  * you have enough backlog. */
 static int min_profit = 2048;
+static int min_count = 1024;
 
 static void substring(const char *str, unsigned len, unsigned cnt)
 {
+    if (cnt < (unsigned) min_count)
+	return;
     if (len == 0)
 	len = strlen(str);
     unsigned profit = cnt * len;
@@ -30,9 +33,11 @@ int prefixes;
 int main(int argc, char **argv)
 {
     enum {
-	OPT_MIN_PROFIT = 256,
+	OPT_MIN_COUNT  = 256,
+	OPT_MIN_PROFIT = 257,
     };
     static const struct option longopts[] = {
+	{ "min-count",        1, 0, OPT_MIN_COUNT },
 	{ "min-profit",       1, 0, OPT_MIN_PROFIT },
 	{ "lines-have-count", 0, &hascnt, 1 },
 	{ "prefixes-only",    0, &prefixes, 1 },
@@ -42,6 +47,10 @@ int main(int argc, char **argv)
     while ((c = getopt_long(argc, argv, "", longopts, NULL)) != -1) {
 	switch (c) {
 	case 0:
+	    break;
+	case OPT_MIN_COUNT:
+	    min_count = atoi(optarg);
+	    assert(min_count > 0);
 	    break;
 	case OPT_MIN_PROFIT:
 	    min_profit = atoi(optarg);
